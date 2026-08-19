a9bb7892c6b9aef5d3c1e396ffcba8b83fdd2edd
Md Tuhin Mahmud <abouttuhin10@gmail.com>
2026-08-20 01:52:28 +0600

    Add files via upload

--- ZLDD_complete_modeling.m
+++ ZLDD_complete_modeling.m
@@ -248,7 +248,7 @@ if ~P.quiet
     end
 
     plot_performance(results);
-    plot_baseline(results, 'figs');   % writes the figure set to ./figs/
+    plot_baseline(results);  
 end
 
 results.runtime_total_s = toc(t_start);
